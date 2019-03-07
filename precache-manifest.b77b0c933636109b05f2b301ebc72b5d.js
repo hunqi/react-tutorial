2eb72ee6f5adc2b8f7420e9e608ddc289aaa5b68
ray.sun <ray.sun@homecredit.cn>
2019-03-07 11:55:07 +0800

    Updates

---
+++ precache-manifest.b77b0c933636109b05f2b301ebc72b5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26643d4426b15bd81771",
+    "url": "/hunqi/react-tutorial/static/js/runtime~main.4d08fd1a.js"
+  },
+  {
+    "revision": "43f0201636ac1e2d2a9c",
+    "url": "/hunqi/react-tutorial/static/js/main.6e905865.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/hunqi/react-tutorial/static/js/2.0f2ac92c.chunk.js"
+  },
+  {
+    "revision": "43f0201636ac1e2d2a9c",
+    "url": "/hunqi/react-tutorial/static/css/main.8261ca21.chunk.css"
+  },
+  {
+    "revision": "2e0fba6ac6d19fd179fa25d1d550f64c",
+    "url": "/hunqi/react-tutorial/index.html"
+  }
+];
